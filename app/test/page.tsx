8da2098714286973b2f42a7b5a778a867061b0a6
Stuart <stuartrobinson3007@gmail.com>
2023-05-25 20:48:11 -0300

    .

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,17 +1,25 @@
-import Link from "next/link.js";
+import Link from "next/link";
+
+export const revalidate = 0;
 
 export default function Page() {
+
+
+    const random = Math.random();
+
+
     return (
         <>
             <div>
-                <Link href="/test/1/modal">
-                    Modal 1
-                </Link>
+                Page: Home {">"} Test
             </div>
-            <div>
-                <Link href="/test/2/modal">
-                    Modal 2
-                </Link>
+
+            <div className="mt-10">
+                Random number: {random}
+            </div>
+
+            <div className="mt-10 text-blue-500">
+                <Link href="/">Navigate back to "Home" page</Link>
             </div>
         </>
     );
